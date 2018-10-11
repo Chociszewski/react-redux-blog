10af4c73dc9bd547835a9544e1538be581523aae
chocisz-iMac <chocisz@PC.local>
2018-10-11 20:32:37 +0200

    Updates

---
+++ precache-manifest.9580706e63cf1dd2c5164748dd1280db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a304f620eb9a1d70514f",
+    "url": "/react-redux-blog/static/js/runtime~main.a304f620.js"
+  },
+  {
+    "revision": "70bcc46d1c2c25aa4e2e",
+    "url": "/react-redux-blog/static/js/main.70bcc46d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-redux-blog/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "70bcc46d1c2c25aa4e2e",
+    "url": "/react-redux-blog/static/css/main.1f9490f7.chunk.css"
+  },
+  {
+    "revision": "ea4725076589328c8d7fd47e1d58b0f0",
+    "url": "/react-redux-blog/index.html"
+  }
+];
